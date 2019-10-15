1f58972cd7b611e3818c6235929f2e388b694a5f
jose luis <cortezluisjose@gmail.com>
2019-10-15 04:41:54 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,13 @@ require 'date'
 
 require 'bundler/setup'
 Bundler.require
-
+class CreateArtists < ActiveRecord::Migration[5.2]
+  def up
+  end
+ 
+  def down
+  end
+end
 
 # put the code to connect to the database here
 
